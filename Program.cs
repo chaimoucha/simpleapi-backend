3efd8ebc5200e06c190a3d7f025f04f68dd1ed49
chaimoucha <chaimatounsi26@gmail.com>
2026-06-05 20:33:33 +0200

    Fix CORS: add AllowCredentials

--- Program.cs
+++ Program.cs
@@ -1,15 +1,10 @@
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllers();
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", policy =>
     {
         policy.WithOrigins("http://localhost:4200", "https://mon-projet-angular-examen.web.app")
               .AllowAnyMethod()
-              .AllowAnyHeader();
+              .AllowAnyHeader()
+              .AllowCredentials();
     });
 });
-var app = builder.Build();
-app.UseCors("AllowAll");
-app.MapControllers();
-app.Run();
