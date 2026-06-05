94bef95e26fbb3c4b6a9ed8be17f8257e699b6cb
chaimoucha <chaimatounsi26@gmail.com>
2026-06-05 20:36:28 +0200

    Fix CORS for Firebase

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", policy =>
     {
         policy.WithOrigins("http://localhost:4200", "https://mon-projet-angular-examen.web.app")
               .AllowAnyMethod()
-              .AllowAnyHeader()
-              .AllowCredentials();
+              .AllowAnyHeader();
     });
 });
+var app = builder.Build();
+app.UseCors("AllowAll");
+app.MapControllers();
+app.Run();
