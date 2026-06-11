1e2d15e3c06903e73bd27923b9b0ccccf1b2f880
chaimoucha <chaimatounsi26@gmail.com>
2026-06-11 10:14:38 +0200

    Fix CORS: add new Firebase URL

--- Program.cs
+++ Program.cs
@@ -4,7 +4,11 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", policy =>
     {
-        policy.WithOrigins("http://localhost:4200", "https://mon-projet-angular-examen.web.app")
+        policy.WithOrigins(
+                "http://localhost:4200",
+                "https://mon-projet-angular-examen.web.app",
+                "https://projet-angular-final.web.app"    // ← AJOUTE CETTE LIGNE
+              )
               .AllowAnyMethod()
               .AllowAnyHeader();
     });
